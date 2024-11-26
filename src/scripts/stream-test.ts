360b6d27aecd468e4f3daafbe38a6f562673372b
Evan McGrane <evanmcgranebb98@gmail.com>
2024-11-26 19:33:19 +0000

    feat: add test example script

--- src/scripts/stream-test.ts
+++ src/scripts/stream-test.ts
@@ -39,41 +39,45 @@ function print({ stream }) {
 	});
 }
 
-const [node1, node2, node3] = await Promise.all([
-	createNode([tcp()], "/ip4/0.0.0.0/tcp/0"),
-	createNode([tcp(), webSockets()], ["/ip4/0.0.0.0/tcp/0", "/ip4/127.0.0.1/tcp/10000/ws"]),
-	createNode([webSockets()], "/ip4/127.0.0.1/tcp/20000/ws"),
-]);
+async function main() {
+	const [node1, node2, node3] = await Promise.all([
+		createNode([tcp()], "/ip4/0.0.0.0/tcp/0"),
+		createNode([tcp(), webSockets()], ["/ip4/0.0.0.0/tcp/0", "/ip4/127.0.0.1/tcp/10000/ws"]),
+		createNode([webSockets()], "/ip4/127.0.0.1/tcp/20000/ws"),
+	]);
 
-printAddrs(node1, "1");
-printAddrs(node2, "2");
-printAddrs(node3, "3");
+	printAddrs(node1, "1");
+	printAddrs(node2, "2");
+	printAddrs(node3, "3");
 
-node1.handle("/print", print);
-node2.handle("/print", print);
-node3.handle("/print", print);
+	node1.handle("/print", print);
+	node2.handle("/print", print);
+	node3.handle("/print", print);
 
-await node1.peerStore.patch(node2.peerId, {
-	multiaddrs: node2.getMultiaddrs(),
-});
-await node2.peerStore.patch(node3.peerId, {
-	multiaddrs: node3.getMultiaddrs(),
-});
-await node3.peerStore.patch(node1.peerId, {
-	multiaddrs: node1.getMultiaddrs(),
-});
+	await node1.peerStore.patch(node2.peerId, {
+		multiaddrs: node2.getMultiaddrs(),
+	});
+	await node2.peerStore.patch(node3.peerId, {
+		multiaddrs: node3.getMultiaddrs(),
+	});
+	await node3.peerStore.patch(node1.peerId, {
+		multiaddrs: node1.getMultiaddrs(),
+	});
 
-// node 1 (TCP) dials to node 2 (TCP+WebSockets)
-const stream = await node1.dialProtocol(node2.peerId, "/print");
-await pipe([uint8ArrayFromString("node 1 dialed to node 2 successfully")], stream);
+	// node 1 (TCP) dials to node 2 (TCP+WebSockets)
+	const stream = await node1.dialProtocol(node2.peerId, "/print");
+	await pipe([uint8ArrayFromString("node 1 dialed to node 2 successfully")], stream);
 
-// node 2 (TCP+WebSockets) dials to node 3 (WebSockets)
-const stream2 = await node2.dialProtocol(node3.peerId, "/print");
-await pipe([uint8ArrayFromString("node 2 dialed to node 3 successfully")], stream2);
+	// node 2 (TCP+WebSockets) dials to node 3 (WebSockets)
+	const stream2 = await node2.dialProtocol(node3.peerId, "/print");
+	await pipe([uint8ArrayFromString("node 2 dialed to node 3 successfully")], stream2);
 
-// node 3 (listening WebSockets) can dial node 1 (TCP)
-try {
-	await node3.dialProtocol(node1.peerId, "/print");
-} catch (err) {
-	console.log("node 3 failed to dial to node 1 with:", err.message);
+	// node 3 (listening WebSockets) can dial node 1 (TCP)
+	try {
+		await node3.dialProtocol(node1.peerId, "/print");
+	} catch (err) {
+		console.log("node 3 failed to dial to node 1 with:", err.message);
+	}
 }
+
+main();
